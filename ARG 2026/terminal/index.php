3d14ad369196600f879b845db3b8f724906ea281
Laura Sofia Heimann <hello@laura.media>
2026-03-17 18:15:45 +0100

    ARG work

---
+++ ARG 2026/terminal/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Speen Orbital OS</title>
+
+        <link rel="stylesheet" href="app.css" />
+    </head>
+    <body>
+        <div class="output">
+        </div>
+        <div class="input-box">
+        user@speen-orbital:~# <input type="text" autocomplete="off" id="input" />
+        </div>
+
+        <div class="overlay"></div>
+        <script src="app.js"></script>
+    </body>
+</html>
